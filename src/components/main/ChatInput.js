acf76e1be84087ccece9fd0adcae54306f34f5f0
Alex Wong <fi0088@wayne.edu>
2019-04-11 09:31:47 -0700

    Change port

--- src/components/main/ChatInput.js
+++ src/components/main/ChatInput.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
 import { FormInput, InputGroup, InputGroupAddon, Button } from 'shards-react';
