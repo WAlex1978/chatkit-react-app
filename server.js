45e849a002324b81b6f1ed8fd3b4ecaff260efbc
Alex Wong <fi0088@wayne.edu>
2019-04-11 10:34:16 -0700

    Scripts update

--- server.js
+++ server.js
@@ -14,6 +14,14 @@ app.use(cors());
 app.use('/api/auth/', authRouter);
 app.use('/api/message/', messageRouter);
 
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('build'));
+}
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(_dirname, 'build', 'index.html'))
+})
+
 module.exports = app.listen(PORT, () => {
 	console.log(`Listening on port ${PORT}`);
 });
